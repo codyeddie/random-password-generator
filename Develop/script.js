d17eb2f55d5fcd8d351dcd5b26715d974df3645a
Cody Eddie <buffbuck0427@gmail.com>
2022-04-05 21:27:07 -0600

    ability to generate password with all characters except special characters

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,8 @@ var generatePassword = function() {
 
   // Create setPassLength function to choose length of password 
     var setPassLength = function () {
-      passLength = prompt("Choose a number between 8 and 12 to determine the length of your password.")
-      if (passLength >= 8 && passLength <= 12) {
+      passLength = prompt("Choose a number between 8 and 128 to determine the length of your password.")
+      if (passLength >= 8 && passLength <= 128) {
         passLength = passLength;
       } else {
         window.alert("You need to enter a vaid number! Please try again.");
@@ -34,14 +34,7 @@ var generatePassword = function() {
   
     // Create variables that contain the value of selections that were made in charOptions function
     var confirmations = charOptions();
-  
-    var lowerCaseChoose = confirmations.lowerCaseChoose;
-    var upperCaseChoose = confirmations.upperCaseChoose;
-    var numericCharChoose = confirmations.numericCharChoose;
-    var specialCharChoose = confirmations.specialCharChoose;
-  
-    
-    
+      
     // Create a function that will create password based on user selection
     var createPassWord = function() {
       // Create variables for the password and the character types that will be used randomly
@@ -53,32 +46,35 @@ var generatePassword = function() {
       var specialChar = "!@#$%^&*()_+=-";
   
       // Add characters to passChars variable base on selections in charOptions function and confirmations objects
-        if (lowerCaseChoose === true) {
+        if (confirmations.lowerCaseChoose === true) {
         passChars += lowerCaseChar;
       }  
-      if (upperCaseChoose === true) {
+      if (confirmations.upperCaseChoose === true) {
         passChars += upperCaseChar;
       }  
-      if (numericCharChoose === true) {
+      if (confirmations.numericCharChoose === true) {
         passChars += numericChar;
       }  
-      if (specialCharChoose === true) {
+      if (confirmations.specialCharChoose === true) {
         passChars =+ specialChar;
       }
       console.log(passChars);
   
-      // Create a password variable for all the selected data to be contained in and a 
-      // for loop to get random characters based on character selections and password length
+      // Create a password variable and loop to iterate through character selections
       var password = "";
       for (var i = 0; i < passLength; i++) {
-        var randomCharacters = Math.floor(Math.random() * passChars.length);
-        password += passChars.substring(randomCharacters, randomCharacters +1);
+        var randomChars = Math.floor(Math.random() * passChars.length);
+        password += passChars.substring(randomChars, randomChars +1);
       }
-    return password;
+      return password;
     }
-
+    
+    // call the create password function within the generatePassword function
     createPassWord();
-  
+
+    // create a password variable outside scope of createPassword() function and return value of password to be used within writePassword function
+    var password = createPassWord();
+    return password;
   }
   
   
@@ -87,7 +83,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -98,4 +94,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-writePassword();
